b4708e3367e48184d34af2bb89ff47122d0b698b
m-harper <matthew.harper@tamu.edu>
2013-01-30 16:55:40 -0600

    Table function declarations

--- Database/Database/Table.h
+++ Database/Database/Table.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <list>
+#include <string>
+#include "Record.h"
+
+enum type {INT, FLOAT, STRING, DATE);
+
 class Table {
 public:
 	Table();
+	Table(std::list<std::string>*, std::list<type>*);
+
+	void add(std::string, type);
+	bool remove(std::string);
+	void insert(Record);
+	std::list<std::string> get_attributes();
+	unsigned get_size();
+	bool rename_attribute(std::string, std::string);
+	void specify_key(std::list<std::string>*);
+	Table cross_join(Table, Table);
+	
+
 	~Table();
 };
 
