d6ac26373aaeb8722e407a0122ddcc69f091eb9b
mharper <matthew.harper@tamu.edu>
2013-02-04 16:47:17 -0600

    Added empty cpp definitions

--- Database/Database/Table.cpp
+++ Database/Database/Table.cpp
@@ -3,6 +3,37 @@
 Table::Table() {
 }
 
+Table::Table(std::list<std::string>* _field, std::list<Type>* _type) {
+
+}
 
 Table::~Table() {
+}
+
+void Table::addAttribute(std::string _name, Type _type) {
+
+}
+
+bool Table::deleteAttribute(std::string _name) {
+	return false;
+}
+
+void Table::insertRecord(Record _record) {
+
+}
+
+std::list<std::string>* Table::getAttributes() { 
+	return NULL;
+}
+
+unsigned Table::getSize() {
+	return 0;
+}
+
+bool Table::renameAttribute(std::string _oldName, std::string _newName) {
+	return false;
+}
+
+Table Table::crossJoin(Table _firstTable, Table _secondTable) {
+	return Table();
 }
