48eece4afd830192582ee429a5d79fcb65d258c2
unknown <mharper@L111C17.cs.tamu.edu>
2013-02-04 16:40:40 -0600

    Created empty cpp and skeleton functions

--- Database/Database/Record.cpp
+++ Database/Database/Record.cpp
@@ -6,3 +6,15 @@ Record::Record() {
 
 Record::~Record() {
 }
+
+std::string Record::accessRecordEntry(int _entry) {
+	return "";
+}
+
+void Record::modifyRecordEntry(int _entry, std::string _newVal) {
+
+}
+
+std::string Record::retrieveRecordEntry(int _entry) {
+	return "";
+}
