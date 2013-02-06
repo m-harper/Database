73cdc2c828c1afd832c25b6fd92dea7c4a74a1fc
m-harper <Igalboc1g4780c>
2013-02-06 17:04:57 -0600

    Changed multiple return types, added constructor, added getEntries

--- Database/Database/Record.cpp
+++ Database/Database/Record.cpp
@@ -1,21 +1,65 @@
 #include "Record.h"
 
+using namespace std;
+
 Record::Record() {
 }
 
+Record::Record(const Record &in) {
+	// Copy all the entries from the record into my tuple
+	for (int i = 0; i < in.getNumEntries(); i++) {
+		tuple.push_back(in.retrieveRecordEntry(i));
+	}
+}
+
+Record::Record(int _entries, std::list<std::string>* _initialValues) {
+
+}
 
 Record::~Record() {
 }
 
-std::string Record::accessRecordEntry(int _entry) {
-	return "";
+// Returns a pointer to the string in the tuple
+//std::string* Record::accessRecordEntry(int _entry) const 
+std::string* Record::accessRecordEntry(int _entry)  {
+	int index = 0;
+	for (list<string>::iterator it = tuple.begin();	it != tuple.end();	it++) {
+		if (index == _entry){
+			return &(*it);
+			//return &(*it);
+		}
+		index++;
+	}
+	return NULL;
 }
 
+// Changes an entry in the tuple at index _entry to _newEntryValue
 void Record::modifyRecordEntry(int _entry, std::string _newEntryValue) {
+	string* str = accessRecordEntry(_entry);
 
+	// Exit if the entry was not found or is out of bounds
+	if (str == NULL)
+		return;
+
+	// Assign the new value
+	*str = _newEntryValue;
 }
 
-std::string Record::retrieveRecordEntry(int _entry) {
-	return "";
+// Returns the string of an entry in the tuple
+std::string Record::retrieveRecordEntry(int _entry) const {
+	int index = 0;
+	for (list<string>::const_iterator it = tuple.begin(); it != tuple.end(); it++) {
+		if (index == _entry) {
+			return *it;
+		}
+	}
+
+	// Return null if entry not found
+	return NULL;
+}
+
+// Returns the number of entries in the tuple
+unsigned Record::getNumEntries() const {
+	return tuple.size();
 }
 
--- Database/Database/Record.h
+++ Database/Database/Record.h
@@ -2,16 +2,32 @@
 #include <list>
 #include <string>
 
+/*  
+	You should have a separate record class that is used to store an individual record (tuple).  
+	A record should consist of an ordered set of strings.  Note that the record should be strings, 
+	though those strings might represent an integer, floating point number, date, or time.   
+	That is, a record on its own has no “knowledge” of what its entries mean, but sees them all 
+	as strings.  Note that strings can be implemented as character arrays or using the string class.  
+	Also, how the record itself is implemented is up to you (e.g. it could be an array, it could 
+	be a container).  The routines that need to be provided for records include:
+		A constructor that allows creation and initialization of an arbitrary number of entries
+        An accessor that allows access to an individual entry in the record
+        Routines (if not provided by the accessor) for modifying (by specifying a new string) 
+			and retrieving (as a string) an entry in the record.
+*/
+
 class Record {
 public:
 	Record();
 	Record(const Record &in);
+	Record(int _entries, std::list<std::string>* _initialValues);
 	~Record();
 
-   std::string accessRecordEntry(int entry);
+   std::string* accessRecordEntry(int entry);
    void modifyRecordEntry(int entry, std::string newEntryValue);
-   std::string retrieveRecordEntry(int entry);
-
+   std::string retrieveRecordEntry(int entry) const;
+   unsigned getNumEntries() const;
+   
 private:
 	std::list<std::string> tuple;
 };
