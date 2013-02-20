6a19bb800621b9aa16a055eabb809717a3744ed3
m-harper <matthew.harper@tamu.edu>
2013-02-20 12:13:36 -0600

    Began parser

--- Database/Database/Database.cpp
+++ Database/Database/Database.cpp
@@ -79,9 +79,139 @@ void Database::deleteRecord(std::string _tableName, std::string _where) {
 	//TODO parse where
 }
 
-/* Parsing requirements:
- *
- *
- *
- *
+/* 
+	Parsing requirements:
+	Query:
+	The query command should be a member function from the database.  It will return a table.  The query command should take in three arguments, a SELECT argument, a FROM argument, and a WHERE argument.  These three arguments should have the following properties
+	In the SELECT argument, allow either
+	A list of which attributes names to keep (the type should remain the same)
+	An indicator (analogous to the * in SQL) to keep all attributes.  You can do this however you want (e.g. a null list of attributes might indicate keep all)
+	In the FROM argument
+	A single table name
+	In the WHERE argument, references to the attribute names.
+	Comparisons of =, !=, >, <, >=, <=  (only between attributes/values of the same type)
+	* An IN operator (given the name of a table with only one attribute)
+	* An EXISTS operator (given the name of a table with only one attribute)
+	AND, OR, and NOT
+	Parentheses.  Note that no more than 3 levels of parentheses need to be supported
+	* Support for arbitrary nesting of parentheses
+	* ALL (given the name of a table with only one attribute)
+	* ANY  (given the name of a table with only one attribute)
+	Note that the WHERE clause will require parsing.  The WHERE argument should be passed in as a string
  */
+
+
+
+Database::token Database::getToken(std::string& _where) {
+	// First check for parenthesis
+	if (_where[0] == '(') {
+		_where = _where.substr(1);
+		return lpar;
+	}
+	if (_where[0] == ')') {
+		_where = _where.substr(1);
+		return rpar;
+	}
+	
+	// Get a token from the string up to the first space
+	std::string piece = _where.substr(0, _where.find(" "));
+
+	if (piece == "=") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return eq;
+	}
+	if (piece == "!=") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return neq;
+	}
+	if (piece == "<") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return lt;
+	}
+	if (piece == "<=") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return lte;
+	}
+	if (piece == ">") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return gt;
+	}
+	if (piece == ">=") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return gte;
+	}
+	if (piece == "IN") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return in;
+	}
+	if (piece == "EXISTS") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return exists;
+	}
+	if (piece == "AND") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return and;
+	}
+	if (piece == "OR") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return or;
+	}
+	if (piece == "NOT") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return not;
+	}
+	if (piece == "ALL") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return all;
+	}
+	if (piece == "ANY") {
+		// Remove the token from the string
+		_where = _where.substr(_where.find(" "));
+		return any;
+	}
+	
+	// Check if the piece is a number
+	bool isNum = true;
+	for (int i = 0; i < piece.length(); i++) {
+		if (!isdigit(piece[i]) && piece[i] != '.') {
+			isNum = false;
+			break;
+		}
+	}
+	
+	if (isNum) {
+		return num;
+	}
+
+	// Everything else has been exhausted, token must be the name of a field
+	return field;
+}
+
+void Database::parse(std::string _where) {
+	Parsed parsed;
+
+	while (_where != "") {
+		switch (getToken(_where)) {
+		case field:
+			// Remove the field from the string
+			break;
+		case num:
+			// Remove the num from the string
+			break;
+		// For all other cases, token has already been removed
+
+		}
+	}
+}
--- Database/Database/Database.h
+++ Database/Database/Database.h
@@ -11,7 +11,7 @@
 class DB_API Database {
 public:
 	Database();
-	Database(const Database &in);
+	Database(const Database&);
 	~Database();
 
 	void addTable(Table table, std::string name);
@@ -22,7 +22,18 @@ public:
 	Table query(std::list<std::string> tableAttributes ,std::string tableName, std::string whereArgument);
 	void deleteRecord(std::string tableName, std::string whereArgument);
 
+	// Parser functions
+	enum token { eq, neq, lt, lte, gt, gte, in, exists, and, or, not, lpar, rpar, all, any, field, num };
+	token getToken(std::string&);
+	void parse(std::string);
+
 private:
 	std::list<std::string> tableNames;
 	std::list<Table> tables;
+
+	// Class for parser ouput
+	class Parsed {
+		std::list<std::string> fields;
+		std::list<std::string> conditions;
+	};
 };
