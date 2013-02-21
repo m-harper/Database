4d4add06e4ed9f45580841abaebcbc86f566afae
m-harper <matthew.harper@tamu.edu>
2013-02-21 15:28:02 -0600

    Rewrote query correctly, added skeleton for when parser is finished
    Note that when adding records to the new table, the attributes of the
    record *MUST* be in the same order as the new table. This property is
    *NOT* certain and must be checked for.

--- Database/Database/Database.cpp
+++ Database/Database/Database.cpp
@@ -60,179 +60,115 @@ std::list<std::string> Database::getNames() const {
 
 Table Database::query(std::list<std::string> _tableAttributes, std::string _tableName, std::string _where) {
 	
-	// Get the index of the requested table
-	int tableIndex = 0;
-	for (std::list<std::string>::iterator it = tableNames.begin(); it != tableNames.end(); it++) {
-		if (*it == _tableName) {
+	// Access the old table based on _tableName
+	Table oldTable;
+	std::list<Table>::iterator tableIterator = tables.begin();
+	for (std::list<std::string>::iterator nameIterator = tableNames.begin(); nameIterator != tableNames.end(); nameIterator++) {
+		if (_tableName == *nameIterator)
 			break;
-		}
-		tableIndex++;
+		// Also increment table iterator
+		tableIterator++;
 	}
+	oldTable = *tableIterator;
 
-	// Get the requested table
-	std::list<Table>::iterator tit = tables.begin();
-	for (int i = 0; i < tableIndex; i++) {
-		tit++;
-	}
-	Table table = *tit;
+	// Construct a new table with the needs attributes based on _tableAttributes
+	Table newTable;
 
-	// Get the index of the requested attribute in the table
-	std::list<int> indices;
-	int attIndex = 0;
-	for (std::list<std::string>::iterator ait = _tableAttributes.begin(); ait != _tableAttributes.end(); ait++) {
-		for (std::list<std::string>::iterator it = table.getFields().begin(); it != table.getFields().end(); it++) {
-			if (*ait == *it) {
-				indices.push_back(attIndex);
+	// Get attributes needed for new table
+	std::list<std::list<std::string>::iterator> neededAttributes;
+	for (std::list<std::string>::iterator paramIter = _tableAttributes.begin(); paramIter != _tableAttributes.end(); paramIter++) {
+		std::list<std::string> attributes = oldTable.getAttributes();
+		for (std::list<std::string>::iterator attIter = attributes.begin(); attIter != attributes.end(); attIter++) {
+			// Attributes are of the form "type name"
+			// Extract the name only
+			std::string name = (*attIter).substr((*attIter).find(" ") + 1);
+			if (name == *paramIter) {
+				neededAttributes.push_back(attIter);
 			}
-			attIndex++;
-		}
-		attIndex = 0;
-	}
-
-	// We now have the correct table and the indicies of the requested fields
-	// We assume that _where is of the form "attribute op value"
-	std::string where = _where;
-	std::string attribute = where.substr(0, where.find(" "));
-	where = where.substr(where.find(" ") + 1);
-	std::string opStr = where.substr(0, where.find(" "));
-	where = where.substr(where.find(" ") + 1);
-	std::string valueString = where;
-	double value = stringToDouble(valueString);
-
-	// Convert the op string to a token
-	token op;
-	if (opStr == "=")
-		op = eq;
-	if (opStr == "!=")
-		op = neq;
-	if (opStr == "<")
-		op = lt;
-	if (opStr == "<=")
-		op = lte;
-	if (opStr == ">")
-		op = gt;
-	if (opStr == ">=")
-		op = gte;
-
-	// Now we have the needed information
-	// Make a new table with the desired qualities
-	Table newTable = Table();
-
-	// Add the attributes to the table
-	// Get the list of all the attributes of the table
-	std::list<std::string> atts = table.getAttributes();
-	// Loop to only select attributes the match the wanted indicies
-	for (std::list<int>::iterator iit = indices.begin(); iit != indices.end(); iit++) {
-		std::string name, fullAtt;
-		Table::Type type; 
-		std::list<std::string>::iterator it = atts.begin();
-		for (int j = 0; j < atts.size(); j++) {
-			if (*iit == j) {
-				// We match a wanted index
-				// Add this attribute to the new table
-				fullAtt = *it;
-				std::string typeStr = fullAtt.substr(0, fullAtt.find(" "));
-				if (typeStr == "0")
-					type = Table::INT;
-				if (typeStr == "1")
-					type = Table::FLOAT;
-				if (typeStr == "3")
-					type = Table::string;
-				if (typeStr == "4")
-					type = Table::DATE;
-
-				name = fullAtt.substr(fullAtt.find(" ") + 1);
-				newTable.addAttribute(name, type);
-			}
-			it++;
-		}
-		// Reset the attribute iterator
-		it = atts.begin();
-	}
-
-	// All the correct attributes have been added
-	// Get the index of the _where attribute in the record/table
-	int recInd = 0;
-	for (std::list<std::string>::iterator it = table.getAttributes().begin(); it != table.getAttributes().end(); it++) {
-		// Split the attribute into type and name, attribute is of form "type name"
-		// All we need is name
-		std::string name = (*it).substr((*it).find(" ") + 1);
-		if (attribute == name) {
-			// Found the required attribute
-			break;
-		}
-		recInd++;
-	}
-
-	// Now add the records that match the condition
-	// Loop through the records of the original table
-	for (std::list<Record>::iterator it = table.getRecords().begin(); it != table.getRecords().end(); it++) {
-		std::string recordEntry = (*it).retrieveRecordEntry(recInd);
-		// Check if the record entry meets the condition
-		switch (op) {
-		case eq:
-			if (recordEntry == valueString) {
-				// Condition met, add record to new table
-				// WE PROBABLY NEED TO MATCH UP WITH THE ATTRIBUTES ALREADY IN THE TABLE, LOOK HERE IF SOMETHING BREAKS
-				std::list<std::string>* initialValues = new std::list<std::string>();
-				initialValues->push_back(recordEntry);
-				newTable.insertRecord(Record(initialValues));
-				delete initialValues;
-			}
-			break;
-		case neq:
-			if (recordEntry != valueString) {
-				// Condition met, add record to new table
-				// WE PROBABLY NEED TO MATCH UP WITH THE ATTRIBUTES ALREADY IN THE TABLE, LOOK HERE IF SOMETHING BREAKS
-				std::list<std::string>* initialValues = new std::list<std::string>();
-				initialValues->push_back(recordEntry);
-				newTable.insertRecord(Record(initialValues));
-				delete initialValues;
-			}
-			break;
-		case lt:
-			if (stringToDouble(recordEntry) < stringToDouble(valueString)) {
-				// Condition met, add record to new table
-				// WE PROBABLY NEED TO MATCH UP WITH THE ATTRIBUTES ALREADY IN THE TABLE, LOOK HERE IF SOMETHING BREAKS
-				std::list<std::string>* initialValues = new std::list<std::string>();
-				initialValues->push_back(recordEntry);
-				newTable.insertRecord(Record(initialValues));
-				delete initialValues;
-			}
-			break;
-		case lte:
-			if (stringToDouble(recordEntry) <= stringToDouble(valueString)) {
-				// Condition met, add record to new table
-				// WE PROBABLY NEED TO MATCH UP WITH THE ATTRIBUTES ALREADY IN THE TABLE, LOOK HERE IF SOMETHING BREAKS
-				std::list<std::string>* initialValues = new std::list<std::string>();
-				initialValues->push_back(recordEntry);
-				newTable.insertRecord(Record(initialValues));
-				delete initialValues;
-			}
-			break;
-		case gt:
-			if (stringToDouble(recordEntry) > stringToDouble(valueString)) {
-				// Condition met, add record to new table
-				// WE PROBABLY NEED TO MATCH UP WITH THE ATTRIBUTES ALREADY IN THE TABLE, LOOK HERE IF SOMETHING BREAKS
-				std::list<std::string>* initialValues = new std::list<std::string>();
-				initialValues->push_back(recordEntry);
-				newTable.insertRecord(Record(initialValues));
-				delete initialValues;
-			}
-			break;
-		case gte:
-			if (stringToDouble(recordEntry) >= stringToDouble(valueString)) {
-				// Condition met, add record to new table
-				// WE PROBABLY NEED TO MATCH UP WITH THE ATTRIBUTES ALREADY IN THE TABLE, LOOK HERE IF SOMETHING BREAKS
-				std::list<std::string>* initialValues = new std::list<std::string>();
-				initialValues->push_back(recordEntry);
-				newTable.insertRecord(Record(initialValues));
-				delete initialValues;
-			}
-			break;
 		}
 	}
 
+	// Add attributes to new table
+	for (std::list<std::list<std::string>::iterator>::iterator it = neededAttributes.begin(); it != neededAttributes.end(); it++) {
+		std::string attribute = **it;
+		// Attribute is of form "type name"
+		// Break the string into type and name
+		std::string typeStr = attribute.substr(0, attribute.find(" "));
+		Table::Type type;
+		if (typeStr == "0")
+			type = Table::INT;
+		if (typeStr == "1")
+			type = Table::FLOAT;
+		if (typeStr == "2")
+			type = Table::string;
+		if (typeStr == "3")
+			type = Table::DATE;
+
+		std::string name = attribute.substr(attribute.find(" ") + 1);
+
+		// Add the attribute to the new table
+		newTable.addAttribute(name, type);
+	}
+
+	// New table is correctly constructed
+	// Loop through records of old table and extract records whose condition matches
+	// First, get the entry number of the attribute in the record
+	// TODO
+	/*
+		std::string attName = // attribute name from _where
+		int index = 0;
+		for (std::list<std::string>::iterator it = oldTable.getAttributes().begin(); it != oldTable.getAttributes().end(); it++) {
+			// Attributes are of the form "type name"
+			// Extract the name only
+			std::string name = (*it).substr((*it).find(" ") + 1);
+			if (name == attName) {
+				// Found the correct entry number
+				break;
+			}
+			index++;
+		}
+
+		// The operation from _where is stored in a token called op
+		Token op; // TODO assign based on parsing
+
+		// Now we know the entry number, begin comparing to condition from _where
+		for (std::list<Record>::iterator recordIter = oldTable.getRecords().begin(); recordIter != oldTable.getRecords.end(); recordIter++) {
+			std::string entry = (*recordIter).retrieveRecordEntry(index);
+			switch (op) {
+			case eq:
+				if (entry == // TODO value from where) {
+
+				}
+				break;
+			case neq:
+				if (entry != // TODO value from where) {
+
+				}
+				break;
+			case lt:
+				if (stringToDouble(entry) < stringToDouble(// TODO value from where)) {
+
+				}
+				break;
+			case lte:
+				if (stringToDouble(entry) <= stringToDouble(// TODO value from where)) {
+
+				}
+				break;
+			case gt:
+				if (stringToDouble(entry) > stringToDouble(// TODO value from where)) {
+
+				}
+				break;
+			case gte:
+				if (stringToDouble(entry) >= stringToDouble(// TODO value from where)) {
+
+				}
+				break;
+			}
+		}
+		
+	*/
 
 	return newTable;
 }
